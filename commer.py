2dca1254d8348ab56719fd7289c8d50daf038af4
Jeffrey Skinner <Jeff@Pyusoft.com>
2022-08-22 18:32:15 -0700

    Python 3.8 := no thanks

--- commer.py
+++ commer.py
@@ -17,7 +17,7 @@ class Commer:
 			self.on_connect.fire(socket)
 			
 			while 1:
-				byte:=socket.recv(BUFFER_SIZE)
+				byte=socket.recv(BUFFER_SIZE)
 				self.on_receive.fire(socket,*deserialize(byte))#tuple unpacking is wrong here because its wrong in serializer (this is living proof that wrong creates more wrong)
 				if not byte:
 					break
